625efc4a0a5c0a2ea70063107d64896535af6fd2
Daria Chugunova <dariachugunova2003@gmail.com>
2025-05-23 19:52:28 +0300

    feat(preprocessing): обновления скрипта предварительной обработки

--- data_preparation.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-# Пример обработки
-df = pd.read_csv('data/drugsComTrain_raw.csv')
-df.dropna(inplace=True)
-df.to_csv('data/train_features_corrected.csv', index=False)
-print("Предобработка завершена")
