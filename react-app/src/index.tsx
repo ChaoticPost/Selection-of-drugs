e049543ea434729f0d0662894c7b442c034ae245
Daria Chugunova <dariachugunova2003@gmail.com>
2025-05-24 14:33:56 +0300

    add: Создание типов данных

--- react-app/src/index.tsx
+++ react-app/src/index.tsx
@@ -1,19 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// Типы для симптомов
+export interface Symptom {
+  id: string;
+  name: string;
+}
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Типы для заболеваний
+export interface Disease {
+  id: string;
+  name: string;
+  description: string;
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// Типы для лекарств
+export interface Medication {
+  id: string;
+  name: string;
+  description: string;
+  dosage: string;
+  sideEffects: string[];
+  effectiveness: number;
+}
+
+// Типы для результатов
+export interface Result {
+  disease: Disease;
+  medications: Medication[];
+}
+
+// Типы для аналитики
+export interface SymptomStat {
+  symptomName: string;
+  count: number;
+}
+
+export interface MedicationStat {
+  medicationName: string;
+  effectiveness: number;
+  reviewCount: number;
+}
+
+export interface DiseaseStat {
+  diseaseName: string;
+  averageEffectiveness: number;
+}
+
+export interface AnalyticsData {
+  symptomStats: SymptomStat[];
+  medicationStats: MedicationStat[];
+  diseaseStats: DiseaseStat[];
+}
