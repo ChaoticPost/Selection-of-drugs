9834ff5a0cc8767db518649b4ddcea8eaddb2bad
Daria Chugunova <dariachugunova2003@gmail.com>
2025-05-24 15:00:56 +0300

    add: Обновление главного компонента

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { AppProvider } from './context/AppContext';
+import HomePage from './pages/HomePage';
+import SymptomInputPage from './pages/SymptomInputPage';
+import ResultsPage from './pages/ResultsPage';
+import AnalyticsPage from './pages/AnalyticsPage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/symptoms" element={<SymptomInputPage />} />
+          <Route path="/results" element={<ResultsPage />} />
+          <Route path="/analytics" element={<AnalyticsPage />} />
+        </Routes>
+      </Router>
+    </AppProvider>
   );
 }
 
